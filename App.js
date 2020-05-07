06124a5f6cee6e6149cf11e846295e30baf598ed
Daniel Maritim <danmatim@yahoo.com>
2020-05-07 21:58:07 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Tweet from './tweet';
+
+function App() {
+  
+
+  return(
+    <div className="app">
+      
+      <Tweet name='dante' message='hi love'/>
+      <Tweet name='pricilar' message='yes love'/>
+      <Tweet name='nathan' message='hi son'/>
+    </div>
+  );
+}
+
+export default App;
