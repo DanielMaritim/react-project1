7d456ad6c91acfbed52cae938536d4fc3ec37188
Daniel Maritim <danmatim@yahoo.com>
2020-05-08 15:24:21 +0300

    first commit about state

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Tweet from './tweet';
 
 function App() {
   
+  const [isRed, setRed] = useState(false);
+  const [count,setCount] = useState(0);
+
+  const Increment = () => {
+    setCount(count + 1);
+    setRed(!isRed);
+  }
 
   return(
     <div className="app">
-      
-      <Tweet name='dante' message='hi love'/>
-      <Tweet name='pricilar' message='yes love'/>
-      <Tweet name='nathan' message='hi son'/>
+      <h2 className={isRed ? 'red' : ''}>change my color</h2>
+      <button onClick={Increment}>Increment</button>
+      <h1>{count}</h1>
+     
     </div>
   );
 }
